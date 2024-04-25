58eb2b1d7e5387bf95d2107bfa8a8654b32968e4
Zhen Yang <zzhenyyang@gmail.com>
2024-04-25 08:28:25 +0800

    added analysis

--- src/app/main/analysis/page.tsx
+++ src/app/main/analysis/page.tsx
@@ -1,20 +1,21 @@
 "use client";
 
 import Button from "@/components/Button";
-import { Campaign, Statistics } from "@/lib/definitions";
+import { Analysis, Campaign, Statistics } from "@/lib/definitions";
 import { generatedImageUrl } from "@/utils/images";
-import { ArrowRight, Check, CircleDollarSign, LineChart, Loader2, LucideEye, MousePointerClick, Percent, ScanEye } from "lucide-react";
-import { collection, deleteDoc, getDocs, query, where, doc as docRef, addDoc, Timestamp, doc, orderBy, QuerySnapshot, DocumentData } from "firebase/firestore";
+import { ArrowLeft, ArrowRight, Check, CircleDollarSign, LineChart, Loader2, LucideEye, MousePointerClick, Percent, ScanEye } from "lucide-react";
+import { collection, query, where, doc as docRef, orderBy, doc, addDoc, updateDoc } from "firebase/firestore";
 import { useCollection, useDocument } from "react-firebase-hooks/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, firestore } from "@/lib/firebase";
 import Image from "next/image";
 import React from "react";
 import { generateDifferenceAnalysis } from "@/actions/generation";
+import toast from "react-hot-toast";
 
 export default function Page() {
   const [user] = useAuthState(auth);
-  const [campaigns, loading, error] = useCollection(query(collection(firestore, "campaigns"), where("email", "==", user!.email), orderBy('timestamp')));
+  const [campaigns, loading, error] = useCollection(query(collection(firestore, "campaigns"), where("email", "==", user!.email), orderBy('version', 'desc')));
 
   if (loading) {
     return (
@@ -24,7 +25,7 @@ export default function Page() {
     );
   }
 
-  if(error) console.error(error)
+  if (error) console.error(error)
 
   return (
     campaigns &&
@@ -37,73 +38,57 @@ export default function Page() {
         <h1 className="text-3xl mb-8 mt-4">Campaign analysis</h1>
       </div>
       <div className="flex justify-center items-center">
-        <div className="max-w-7xl">
-          {campaigns.docs?.map(async(campaign, index) => {
+        <div className="max-w-7xl px-14">
+          {campaigns.docs?.map((campaign, index) => {
             const data = campaign.data() as Campaign;
+            const prevCampaign = campaigns.docs[index + 1]
+
+            // if (data.analysisId && data.version != 0) {
+            //   // Generate analysis
+            //   const generateAnalysis = async () => {
+            //     await generateDifferenceAnalysis(data, prevCampaign.data() as Campaign)
+            //     const analysisDoc = await addDoc(collection(firestore, "analysis"), {
+            //       campaignId1: campaign.id,
+            //       campaignId2: prevCampaign.id,
+            //       difference: "Generate difference",
+            //       suggestions: "Generate suggestions"
+            //     })
+            //     await updateDoc(doc(firestore, "campaigns", campaign.id), { analysisId: analysisDoc.id })
+            //   }
+
+            //   try {
+            //     generateAnalysis();
+            //   } catch (err) {
+            //     console.error(err)
+            //     toast.error("Analysis cannot be generated at this time.")
+            //   }
+            // }
 
             return (
-              campaigns.docs[index + 1] &&
+              prevCampaign &&
               <div className="flex gap-8 items-center border-gray-400 py-20" key={index}>
                 <div className="min-w-60 flex-[1]">
-                  <h3>Version {index}</h3>
-                  {!data.generatedImage ?
-                    <Image
-                      src={'/image-loading.png'}
-                      alt="Placeholder image"
-                      width={500}
-                      height={500}
-                    />
-                    :
-                    <img
-                      alt="Generated image"
-                      className="w-full"
-                      src={generatedImageUrl(data.generatedImage.split("/")[1])}
-                    />
-                  }
-                  <CampaignStats campaignId={campaign.id} key={campaign.id} />
+                  <h3>Version {data.version - 1}</h3>
+                  <CampaignStats
+                    campaignId={prevCampaign.id}
+                    key={prevCampaign.id}
+                    generatedImage={prevCampaign.data().generatedImage}
+                  />
                 </div>
                 <ArrowRight size={30} className="min-w-8" key={campaign.id} />
                 <div className="min-w-60 flex-[1]">
-                  <h3>Version {index + 1}</h3>
-                  {!campaigns.docs[index + 1].data().generatedImage ?
-                    <Image
-                      src={'/image-loading.png'}
-                      alt="Placeholder image"
-                      width={500}
-                      height={500}
-                    />
-                    :
-                    <img
-                      alt="Generated image"
-                      className="w-full"
-                      src={generatedImageUrl(campaigns.docs[index + 1].data().generatedImage.split("/")[1])}
-                    />
-                  }
-                  <CampaignStats campaignId={campaigns.docs[index + 1].id} key={campaigns.docs[index + 1].id} />
+                  <h3>Version {data.version}</h3>
+                  <CampaignStats
+                    campaignId={campaign.id}
+                    key={campaign.id}
+                    generatedImage={data.generatedImage}
+                  />
                 </div>
-                <div className="flex flex-col gap-6 justify-center">
+                <div className="flex flex-col gap-6 justify-center flex-[2]">
                   <div className="ml-10 border border-gray-400 rounded-md p-6">
-                    {/* {await generateDifferenceAnalysis(data, campaigns.docs[index + 1].data() as Campaign)} */}
-                    {/* {getDocs(query(
-                      collection(firestore, "comparisons"), 
-                      where("campaignId1", "==", campaign.id), 
-                      where("campaignId2", "==", campaigns.docs[index + 1].id)
-                    )).then(comparison => (
-                      <>
-                      <h3 className="font-bold">Difference analysis</h3>
-                      <p>
-                        {comparison.docs[0].data().difference}
-                      </p>
-                      <h3 className="font-bold mt-4">Suggestions</h3>
-                      <p>
-                        {comparison.docs[0].data().suggestions}
-                      </p>
-                      </>
-                    ))} */}
-                    <h3 className="font-bold">Difference analysis</h3>
-                    Our solution? Leveraging generative AI to efficiently produce and track advertising materials. By utilizing state-of-the-art technologies like Gemini Pro and Gemini Pro Vision for accurate insights, we streamline the process.
-                    <h3 className="font-bold mt-4">Suggestions</h3>
-                    Good morning, judges. Our project, Rapid Marketing, addresses a common challenge faced by startups: limited capital for advertising experimentation. Traditional methods like illustrations and A/B testing are costly and prone to inaccuracies.
+                    {data.analysisId &&
+                      <VersionAnalysis analysisId={data.analysisId} />
+                    }
                   </div>
                 </div>
               </div>
@@ -115,29 +100,70 @@ export default function Page() {
   )
 }
 
-function CampaignStats({ campaignId }: { campaignId: string }) {
+function CampaignStats({ campaignId, generatedImage }: { campaignId: string; generatedImage: string; }) {
   const [statistics, loading, error] = useCollection(query(collection(firestore, "statistics"), where("campaignId", "==", campaignId)));
   const data = statistics?.docs[0]?.data() as Statistics;
 
   return (
     data &&
-    <div className="flex flex-col gap-3 p-4 mt-4 rounded-md bg-white">
-      <p className="flex gap-2">
-        <MousePointerClick /> Clicks
-        <span className="font-bold ml-auto">{data.clicks}</span>
-      </p>
-      <p className="flex gap-2">
-        <ScanEye /> Impressions
-        <span className="font-bold ml-auto">{data.impressions}</span>
-      </p>
-      <p className="flex gap-2">
-        <Percent /> Conversion rate
-        <span className="font-bold ml-auto">{data.conversionRate}%</span>
-      </p>
-      <p className="flex gap-2">
-        <CircleDollarSign /> Costs
-        <span className="font-bold ml-auto">${data.totalCost}</span>
-      </p>
-    </div>
+    <>
+      {!generatedImage ?
+        <Image
+          src={'/image-loading.png'}
+          alt="Placeholder image"
+          width={500}
+          height={500}
+        />
+        :
+        <img
+          alt="Generated image"
+          className="w-full"
+          src={generatedImageUrl(generatedImage.split("/")[1])}
+        />
+      }
+      <div className="flex flex-col gap-3 p-4 mt-4 rounded-md bg-white">
+        <p className="flex gap-2">
+          <MousePointerClick /> Clicks
+          <span className="font-bold ml-auto">{data.clicks}</span>
+        </p>
+        <p className="flex gap-2">
+          <ScanEye /> Impressions
+          <span className="font-bold ml-auto">{data.impressions}</span>
+        </p>
+        <p className="flex gap-2">
+          <Percent /> Conversion rate
+          <span className="font-bold ml-auto">{data.conversionRate}%</span>
+        </p>
+        <p className="flex gap-2">
+          <CircleDollarSign /> Costs
+          <span className="font-bold ml-auto">${data.totalCost}</span>
+        </p>
+      </div>
+    </>
+  )
+}
+
+function VersionAnalysis({ analysisId }: { analysisId: string }) {
+  const [analysis, loading, error] = useDocument(doc(firestore, "analysis", analysisId));
+  const data = analysis?.data() as Analysis
+
+  if (loading) {
+    return (
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Loader2 className="animate-spin" />
+      </div>
+    );
+  }
+
+  if (error) console.error(error)
+
+  return (
+    data &&
+    <>
+      <h3 className="font-bold">Difference analysis</h3>
+      {data.difference}
+      <h3 className="font-bold mt-4">Suggestions</h3>
+      {data.suggestions}
+    </>
   )
 }
--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,7 +1,8 @@
 import { Timestamp } from "firebase/firestore";
 
 export type Campaign = {
-  brandId: string
+  brandId: string;
+  version: number;
   size: string;
   projectDescription: string;
   targetAudience: string;
@@ -10,13 +11,22 @@ export type Campaign = {
   callToAction: string;
   exampleImage: string;
   generatedImage: string;
+  analysisId: string;
 }
 
 export type Statistics = {
-  campaignId: string, 
-  clicks: number,
-  impressions: number,
-  conversionRate: number,
-  totalCost: number,
-  timestamp: Timestamp
+  campaignId: string;
+  clicks: number;
+  impressions: number;
+  conversionRate: number;
+  totalCost: number;
+  timestamp: Timestamp;
+}
+
+export type Analysis = {
+  analysisId: string;
+  campaignId1: string;
+  campaignId2: string;
+  difference: string;
+  suggestions: string;
 }
